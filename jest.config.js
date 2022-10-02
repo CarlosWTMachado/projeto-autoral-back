76a976787edc4e15c83915027e88311204a8edb4
Carlos W T Machado <carloswtmachado@gmail.com>
2022-10-02 06:00:20 -0300

    test: sign up unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,30 @@
 /* eslint-disable */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	verbose: true,
+	extensionsToTreatAsEsm: ['.ts'],
+	globals: {
+		'ts-jest': {
+			useESM: true,
+		},
+	},
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
+	collectCoverage: true,
+	coveragePathIgnorePatterns: [
+		'node_modules',
+		'interfaces',
+		'repositories',
+		'<rootDir>/src/repositories',
+		'<rootDir>/src/utils',
+		'<rootDir>/src/middlewares',
+		'<rootDir>/src/schemas',
+		'<rootDir>/src/database.ts',
+		'<rootDir>/src/errors',
+		'<rootDir>/tests/factories',
+		'<rootDir>/src/app.ts',
+	]
 };
