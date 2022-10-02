7d566f68507a00011c97472267a4fe5fb232808a
Carlos W T Machado <carloswtmachado@gmail.com>
2022-10-02 03:56:01 -0300

    feat: encrypt password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,7 +4,7 @@ import authService from '../services/authService';
 export async function signUp(req: Request, res: Response) {
 	const body = req.body;
 	await authService.signUp(body);
-	return res.sendStatus(200);
+	return res.sendStatus(201);
 }
 
 export default {
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import error from "../errors/error";
 import { signUpInterface } from "../types/signUpInterface";
 import standardizePhone from "../utils/standardizePhone";
 import standardizeCep from "../utils/standardizeCep";
+import encryptPassword from "../utils/encryptPassword";
 import cepService from "./cepService";
 import addressService from "./addressService";
 import userService from "./userService";
@@ -22,9 +23,10 @@ export async function signUp(data: signUpInterface) {
 	};
 	const { id: addressId } = await addressService.create(address);
 	// register user 
+	const encryptedPassword = encryptPassword(data.password);
 	const user = {
 		email: data.email,
-		password: data.password,
+		password: encryptedPassword,
 		fullName: data.fullName,
 		addressId
 	};
