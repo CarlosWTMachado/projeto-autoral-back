52632c391c03cb5ce4a21eafbca7e4daa683ed40
Carlos W T Machado <carloswtmachado@gmail.com>
2022-10-06 07:08:07 -0300

    feat: pet route

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import authController from '../controllers/authController';
+import petController from '../controllers/petController';
 import schemaValidate from '../middlewares/handleSchemas';
 import petSchema from '../schemas/petSchema';
 import validateToken from '../middlewares/validateToken';
 
 const petRouter = Router();
 
-petRouter.post('/pet', validateToken, schemaValidate(petSchema), authController.signUp);
+petRouter.post('/pet', validateToken, schemaValidate(petSchema), petController.register);
 // petRouter.get('/pet');
 // petRouter.put('/pet');
 // petRouter.delete('/pet');
